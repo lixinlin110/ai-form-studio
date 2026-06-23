27a9e380410ccf878e1ae74f6ce03cdc1e230755
lixinlin <1144769922@qq.com>
2026-06-23 17:01:24 +0800

    Improve install reproducibility and bundle size

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,70 @@
 import App from './App.vue'
-import ElementPlus from 'element-plus'
+import {
+  ElAside,
+  ElButton,
+  ElCheckbox,
+  ElCheckboxGroup,
+  ElContainer,
+  ElDatePicker,
+  ElDivider,
+  ElForm,
+  ElFormItem,
+  ElHeader,
+  ElIcon,
+  ElInput,
+  ElInputNumber,
+  ElMain,
+  ElMenu,
+  ElMenuItem,
+  ElOption,
+  ElRadio,
+  ElRadioButton,
+  ElRadioGroup,
+  ElRate,
+  ElSelect,
+  ElSkeleton,
+  ElStep,
+  ElSteps,
+  ElSwitch,
+  ElTag,
+} from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import router from './router'
 import './style.css'
 
-createApp(App).use(createPinia()).use(router).use(ElementPlus).mount('#app')
+const app = createApp(App)
+const elementPlusComponents = [
+  ElAside,
+  ElButton,
+  ElCheckbox,
+  ElCheckboxGroup,
+  ElContainer,
+  ElDatePicker,
+  ElDivider,
+  ElForm,
+  ElFormItem,
+  ElHeader,
+  ElIcon,
+  ElInput,
+  ElInputNumber,
+  ElMain,
+  ElMenu,
+  ElMenuItem,
+  ElOption,
+  ElRadio,
+  ElRadioButton,
+  ElRadioGroup,
+  ElRate,
+  ElSelect,
+  ElSkeleton,
+  ElStep,
+  ElSteps,
+  ElSwitch,
+  ElTag,
+]
+
+elementPlusComponents.forEach((component) => app.use(component))
+
+app.use(createPinia()).use(router).mount('#app')
